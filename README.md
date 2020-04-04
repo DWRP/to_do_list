07d8bea0f6f4fc6f157bbb66da6bed80bef29387
Douglas Walas <douglas.skd@gmail.com>
2020-04-03 21:03:07 -0300

    Finish App and create readme

--- README.md
+++ README.md
@@ -1,16 +1,24 @@
 # todo_list
 
-A new Flutter project.
+Flutter project of Udemy Course by Daniel Ciolfi.
+First application in the course.
 
-## Getting Started
+## Objective
 
-This project is a starting point for a Flutter application.
+- Read and write json files in local database
+- Learning about ListViwer and Builder
+- Async responses and requests
 
-A few resources to get you started if this is your first Flutter project:
+## Resume
 
-- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
+  Leaning about flutter and your structure.
+  This Application is a simple To Do List with
+  write and delete items methods.
 
-For help getting started with Flutter, view our
-[online documentation](https://flutter.dev/docs), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+## Getting Start
+
+  For this project, is necessary install flutter in path and flutter
+  extensions in your IDE(Android Studio) or Editor (Visual Studio Code).
+  
+  I'm using flutter run in terminal. Flutter dir and Flutter/bin need is
+  in PATH Variables (system preferencially)
--- lib/main.dart
+++ lib/main.dart
@@ -23,6 +23,10 @@ class _HomeState extends State<Home> {
   
   List _toDoList = [];
 
+  Map<String, dynamic> _lastRemoved;
+  
+  int _lastRemovedPos;
+
   @override
   void initState(){
     super.initState();
@@ -31,8 +35,21 @@ class _HomeState extends State<Home> {
       setState(() {
         _toDoList = json.decode(data);
       });
+    }); 
+  }
+
+  Future<Null> _refresh() async{
+    await Future.delayed(Duration(seconds: 1));
+    setState(() {
+      _toDoList.sort((index,next){
+        if (index["conclued"] && !next["conclued"]) return 1;
+        else if (!index["conclued"] && next["conclued"]) return -1;
+        else return 0;
+      });
+      saveData(_toDoList);
     });
-    
+
+    return null;
   }
 
   @override
@@ -77,29 +94,69 @@ class _HomeState extends State<Home> {
           ),
 
           Expanded(
-            child: ListView.builder(
-              padding: EdgeInsets.only(top:10),
-              itemCount: _toDoList.length,
-              itemBuilder: (contex,index){
-                return CheckboxListTile(
-                  title: Text(_toDoList[index]["title"]),
-                  value: _toDoList[index]["conclued"],
-                  secondary: CircleAvatar(
-                    child: Icon(_toDoList[index]["conclued"]?Icons.cached:Icons.error,color: Colors.purple,),
-                  ),
-                  onChanged:(cheched){
-                    setState(() {
-                      _toDoList[index]["conclued"] = cheched;
-                      saveData(_toDoList);
-                    });
-                  },
-                );
-              }
-              )
+            child: RefreshIndicator(
+              child: ListView.builder(
+                padding: EdgeInsets.only(top:10),
+                itemCount: _toDoList.length,
+                itemBuilder: buildItem
+              ), 
+              onRefresh: _refresh)
           )
         ],
-        ),
+      ),
     );
+  }
+
+  Widget buildItem(BuildContext context,int index){
+    return Dismissible(
+        background: Container(
+          color: Colors.red,
+          child: Align(
+            alignment: Alignment(-0.9, 0),
+            child: Icon(Icons.delete,color:Colors.white),
+          ),
+        ),
+        direction: DismissDirection.startToEnd,
+        key: Key(DateTime.now().millisecondsSinceEpoch.toString()), 
+        child: CheckboxListTile(
+          title: Text(_toDoList[index]["title"]),
+          value: _toDoList[index]["conclued"],
+          secondary: CircleAvatar(
+            child: Icon(_toDoList[index]["conclued"]?Icons.done:Icons.error,color: Colors.purple,),
+          ),
+          onChanged:(cheched){
+            setState(() {
+              _toDoList[index]["conclued"] = cheched;
+              saveData(_toDoList);
+            });
+          },
+        ),
+        onDismissed: (direction){
+          setState(() {
+            _lastRemoved = Map.from(_toDoList[index]);
+            _lastRemovedPos = index;
+            _toDoList.removeAt(index);
+            saveData(_toDoList);
+
+            final snack = SnackBar(
+              content: Text('Task \"${_lastRemoved["title"]}\" removed!'),
+              action: SnackBarAction(
+                label: "Dimiss",
+                onPressed: (){
+                  setState(() {
+                    _toDoList.insert(_lastRemovedPos, _lastRemoved);
+                  });
+                },
+                ),
+                duration: Duration(seconds:3),
+            );
+            
+            Scaffold.of(context).removeCurrentSnackBar();
+            Scaffold.of(context).showSnackBar(snack);
+          });
+
+        },
+      );
   }
 }
 
